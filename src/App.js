5f82f6314a1b198f73192ae4112e62eb234bc0f8
Enoch2k2 <enoch2k2@hotmail.com>
2021-07-08 11:27:35 -0500

    added state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+import React, { useState } from 'react';
 import NavBar from './components/NavBar';
 import BlogForm from './components/BlogForm';
 import BlogList from './components/BlogList';
 
-const blogs = [
-  { title: "title 1", author: "author 1", content: "content 1" },
-  { title: "title 2", author: "author 2", content: "content 2" },
-  { title: "title 3", author: "author 3", content: "content 3" },
-  { title: "title 4", author: "author 4", content: "content 4" },
-  { title: "title 5", author: "author 5", content: "content 5" }
-]
-
 function App() {
+  const [ blogs, setBlogs ] = useState([
+    { title: "title 1", author: "author 1", content: "content 1" },
+    { title: "title 2", author: "author 2", content: "content 2" },
+    { title: "title 3", author: "author 3", content: "content 3" },
+    { title: "title 4", author: "author 4", content: "content 4" },
+    { title: "title 5", author: "author 5", content: "content 5" }
+  ])
+
+  const addBlog = blog => {
+    const newBlogs = [...blogs, blog];
+    setBlogs(newBlogs);
+  }
+
   return (
     <div className="App">
       <NavBar />
 
       <h1>Welcome to Blogger</h1>
 
-      <BlogForm />
+      <BlogForm addBlog={ addBlog } header="Create Blog!!!!!" />
 
       <BlogList blogs={ blogs } />
 
--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,19 +1,47 @@
-const BlogForm = (props) => {
+import React, { useState } from 'react';
+
+const BlogForm = ({ addBlog, header }) => {
+  const [ blog, setBlog ] = useState({
+    title: '',
+    content: '',
+    author: ''
+  })
+
+  const handleChange = event => {
+    setBlog({
+      ...blog,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    addBlog(blog)
+    
+    setBlog({
+      title: '',
+      content: '',
+      author: ''
+    })
+
+  }
+
   return (
     <>
-      <h3>Create Blog</h3>
-      <form>
+      <h3>{ header }</h3>
+      <form onSubmit={ handleSubmit }>
         <div>
           <label>Title</label>
-          <input type="text" />
+          <input type="text" name="title" value={ blog.title } onChange={ handleChange } required={true} />
         </div>
         <div>
           <label>Author</label>
-          <input type="text" />
+          <input type="text" name="author" value={ blog.author } onChange={ handleChange } />
         </div>
         <div>
           <label>Content</label><br/>
-          <textarea />
+          <textarea name="content" value={ blog.content } onChange={ handleChange }/>
         </div>
 
         <input type="submit" value="Create Blog" />
