ad62926f69518c7d599bb45ec7617b40c3c60b0e
Enoch2k2 <enoch2k2@hotmail.com>
2021-07-15 10:56:02 -0500

    changed to class components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import BlogForm from './components/BlogForm';
 import BlogList from './components/BlogList';
 import Blog from './components/Blog';
 
-function App() {
-  const [ blogs, setBlogs ] = useState([])
+import React, { Component } from 'react'
 
-  useEffect(() => {
-    // fetch our blogs
+export class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      blogs: []
+    }
+  }
+
+  componentDidMount() {
     fetch('http://localhost:3001/blogs')
       .then(resp => resp.json())
-      .then(blogs => setBlogs(blogs))
-  }, [])
+      .then(blogs => this.setState({ blogs }))
+  }
 
-  const addBlog = (blog, history) => {
+  addBlog = (blog, history) => {
 
     fetch('http://localhost:3001/blogs', {
       method: "POST",
@@ -27,55 +35,131 @@ function App() {
     })
       .then(resp => resp.json())
       .then(blog => {
-        const newBlogs = [...blogs, blog];
-        setBlogs(newBlogs);
+        const newBlogs = [...this.state.blogs, blog];
+        this.setState({ blogs: newBlogs });
         history.push('/blogs')
       })
   }
 
-  return (
-    <Router>
-      <div className="App">
-
-        <NavBar />
-
-        <h1>Welcome to Blogger</h1>
-        <Switch>
-          <Route 
-            exact path="/"
-            render={ 
-              (props) => <BlogList { ...props } blogs={ blogs } />
-            }
-          />
-          <Route 
-            exact path="/blogs"
-            render={ 
-              (props) => <BlogList { ...props } blogs={ blogs } />
-            }
-          />
-          <Route 
-            exact path="/blogs/new"
-            render={
-              (props) => <BlogForm 
-                          { ...props }
-                          addBlog={ addBlog }
-                          header="Create Blog!!!!!"
-                        /> 
-            }
-          />
-          <Route
-            exact path="/blogs/:id"
-            component={ Blog }
-          />
-
-          <Route
-            render={() => <h1>Page Not Found</h1>}
-          />
-        </Switch>
-        <footer>Blogger Componany Inc. All rights reserved.</footer>
-      </div>
-    </Router>
-  );
+  render() {
+    return (
+      <Router>
+        <div className="App">
+  
+          <NavBar />
+  
+          <h1>Welcome to Blogger</h1>
+          <Switch>
+            <Route 
+              exact path="/"
+              render={ 
+                (props) => <BlogList { ...props } blogs={ this.state.blogs } />
+              }
+            />
+            <Route 
+              exact path="/blogs"
+              render={ 
+                (props) => <BlogList { ...props } blogs={ this.state.blogs } />
+              }
+            />
+            <Route 
+              exact path="/blogs/new"
+              render={
+                (props) => <BlogForm 
+                            { ...props }
+                            addBlog={ this.addBlog }
+                            header="Create Blog!!!!!"
+                          />
+              }
+            />
+            <Route
+              exact path="/blogs/:id"
+              component={ Blog }
+            />
+  
+            <Route
+              render={() => <h1>Page Not Found</h1>}
+            />
+          </Switch>
+          <footer>Blogger Componany Inc. All rights reserved.</footer>
+        </div>
+      </Router>
+    );
+  }
 }
+
+
+// function App() {
+//   const [ blogs, setBlogs ] = useState([])
+
+//   useEffect(() => {
+//     // fetch our blogs
+//     fetch('http://localhost:3001/blogs')
+//       .then(resp => resp.json())
+//       .then(blogs => setBlogs(blogs))
+//   }, [])
+
+//   const addBlog = (blog, history) => {
+
+//     fetch('http://localhost:3001/blogs', {
+//       method: "POST",
+//       headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify( blog )
+//     })
+//       .then(resp => resp.json())
+//       .then(blog => {
+//         const newBlogs = [...blogs, blog];
+//         setBlogs(newBlogs);
+//         history.push('/blogs')
+//       })
+//   }
+
+//   return (
+//     <Router>
+//       <div className="App">
+
+//         <NavBar />
+
+//         <h1>Welcome to Blogger</h1>
+//         <Switch>
+//           <Route 
+//             exact path="/"
+//             render={ 
+//               (props) => <BlogList { ...props } blogs={ blogs } />
+//             }
+//           />
+//           <Route 
+//             exact path="/blogs"
+//             render={ 
+//               (props) => <BlogList { ...props } blogs={ blogs } />
+//             }
+//           />
+//           <Route 
+//             exact path="/blogs/new"
+//             render={
+//               (props) => <BlogForm 
+//                           { ...props }
+//                           addBlog={ addBlog }
+//                           header="Create Blog!!!!!"
+//                         />
+//             }
+//           />
+//           <Route
+//             exact path="/blogs/:id"
+//             component={ Blog }
+//           />
+
+//           <Route
+//             render={() => <h1>Page Not Found</h1>}
+//           />
+//         </Switch>
+//         <footer>Blogger Componany Inc. All rights reserved.</footer>
+//       </div>
+//     </Router>
+//   );
+// }
 
 export default App;
--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,55 +1,117 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
-const BlogForm = ({ addBlog, header, history, match, location }) => {
-  const [ blog, setBlog ] = useState({
-    title: '',
-    content: '',
-    author: ''
-  })
+class BlogForm extends Component {
+  constructor(props) {
+    super(props);
 
-  const handleChange = event => {
-    setBlog({
-      ...blog,
-      [event.target.name]: event.target.value
-    })
+    this.state = {
+      title: '',
+      content: '',
+      author: ''
+    }
   }
 
-  const handleSubmit = event => {
+  handleSubmit = event => {
     event.preventDefault();
+
+    const { history, match, location, addBlog } = this.props;
     console.log('history', history)
     console.log('match', match)
     console.log('location', location)
-    addBlog(blog, history)
+    addBlog(this.state, history)
     
-    setBlog({
+    this.setState({
       title: '',
       content: '',
       author: ''
     })
-
   }
 
-  return (
-    <>
-      <h3>{ header }</h3>
-      <form onSubmit={ handleSubmit }>
-        <div>
-          <label>Title</label>
-          <input type="text" name="title" value={ blog.title } onChange={ handleChange } required={true} />
-        </div>
-        <div>
-          <label>Author</label>
-          <input type="text" name="author" value={ blog.author } onChange={ handleChange } />
-        </div>
-        <div>
-          <label>Content</label><br/>
-          <textarea name="content" value={ blog.content } onChange={ handleChange }/>
-        </div>
+  handleChange = event => {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value
+    })
+  }
 
-        <input type="submit" value="Create Blog" />
-      </form>
-    </>
-  )
+  render() {
+    const { header } = this.props;
+    const { title, author, content } = this.state;
+    return (
+        <>
+        <h3>{ header }</h3>
+        <form onSubmit={ this.handleSubmit }>
+          <div>
+            <label>Title</label>
+            <input type="text" name="title" value={ title } onChange={ this.handleChange } required={true} />
+          </div>
+          <div>
+            <label>Author</label>
+            <input type="text" name="author" value={ author } onChange={ this.handleChange } />
+          </div>
+          <div>
+            <label>Content</label><br/>
+            <textarea name="content" value={ content } onChange={ this.handleChange }/>
+          </div>
+  
+          <input type="submit" value="Create Blog" />
+        </form>
+      </>
+    )
+  }
 }
+
+
+// const BlogForm = ({ addBlog, header, history, match, location }) => {
+//   const [ blog, setBlog ] = useState({
+//     title: '',
+//     content: '',
+//     author: ''
+//   })
+
+//   const handleChange = event => {
+//     setBlog({
+//       ...blog,
+//       [event.target.name]: event.target.value
+//     })
+//   }
+
+  // const handleSubmit = event => {
+  //   event.preventDefault();
+  //   console.log('history', history)
+  //   console.log('match', match)
+  //   console.log('location', location)
+  //   addBlog(blog, history)
+    
+  //   setBlog({
+  //     title: '',
+  //     content: '',
+  //     author: ''
+  //   })
+
+//   }
+
+  // return (
+  //   <>
+  //     <h3>{ header }</h3>
+  //     <form onSubmit={ handleSubmit }>
+  //       <div>
+  //         <label>Title</label>
+  //         <input type="text" name="title" value={ blog.title } onChange={ handleChange } required={true} />
+  //       </div>
+  //       <div>
+  //         <label>Author</label>
+  //         <input type="text" name="author" value={ blog.author } onChange={ handleChange } />
+  //       </div>
+  //       <div>
+  //         <label>Content</label><br/>
+  //         <textarea name="content" value={ blog.content } onChange={ handleChange }/>
+  //       </div>
+
+  //       <input type="submit" value="Create Blog" />
+  //     </form>
+  //   </>
+  // )
+// }
 
 export default BlogForm;
